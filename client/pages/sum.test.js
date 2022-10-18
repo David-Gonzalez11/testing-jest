a231cedefd63045d7d051dbf4d082885e115f407
David Gonzalez <Davidgonzalez_1999@yahoo.com>
2022-10-18 03:00:10 +0000

    In progress of testing more

---
+++ client/pages/sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('./sum');
+const subtract = require('./subtract');
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('subtract 3 - 2 to equal 1', () => {
+  expect(subtract(3, 2)).toBe(1);
+});
